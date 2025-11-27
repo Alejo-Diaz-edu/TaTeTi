cbaa955e44ede999139dc9c4499f4c1cefaeca15
Anto-nicoletti <Antonio.Nicoletti@protonmail.com>
2025-11-27 15:53:37 -0300

    termine el bolque lo simule  funciona bien

--- almacenamiento de jugada/simulation/qsim/work/@bloque_final/_primary.vhd
+++ almacenamiento de jugada/simulation/qsim/work/@bloque_final/_primary.vhd
@@ -9,26 +9,10 @@ entity Bloque_final is
         CLK             : in     vl_logic;
         data            : in     vl_logic_vector(3 downto 0);
         Habilitada_ocupado: out    vl_logic;
-        Empate          : out    vl_logic;
-        Ganador_jugador_uno: out    vl_logic;
-        Ganador_jugador_dos: out    vl_logic;
-        pin_name2       : out    vl_logic;
-        pin_name3       : out    vl_logic;
-        pin_name4       : out    vl_logic;
-        pin_name5       : out    vl_logic;
-        pin_name6       : out    vl_logic;
-        pin_name8       : out    vl_logic;
-        pin_name9       : out    vl_logic;
-        pin_name10      : out    vl_logic;
-        pin_name11      : out    vl_logic;
-        pin_name12      : out    vl_logic;
-        pin_name13      : out    vl_logic;
-        pin_name14      : out    vl_logic;
-        pin_name15      : out    vl_logic;
-        pin_name16      : out    vl_logic;
-        pin_name17      : out    vl_logic;
-        pin_name18      : out    vl_logic;
-        pin_name19      : out    vl_logic;
-        pin_name20      : out    vl_logic
+        P1_LEDS         : out    vl_logic_vector(0 to 8);
+        P2_LEDS         : out    vl_logic_vector(0 to 8);
+        Ganador_jugador_1: out    vl_logic;
+        Ganador_jugador_2: out    vl_logic;
+        Empate          : out    vl_logic
     );
 end Bloque_final;
--- almacenamiento de jugada/simulation/qsim/work/@bloque_final_vlg_check_tst/_primary.vhd
+++ almacenamiento de jugada/simulation/qsim/work/@bloque_final_vlg_check_tst/_primary.vhd
@@ -4,27 +4,11 @@ entity Bloque_final_vlg_check_tst is
     port(
         Empate          : in     vl_logic;
         Fin             : in     vl_logic;
-        Ganador_jugador_dos: in     vl_logic;
-        Ganador_jugador_uno: in     vl_logic;
+        Ganador_jugador_1: in     vl_logic;
+        Ganador_jugador_2: in     vl_logic;
         Habilitada_ocupado: in     vl_logic;
-        pin_name2       : in     vl_logic;
-        pin_name3       : in     vl_logic;
-        pin_name4       : in     vl_logic;
-        pin_name5       : in     vl_logic;
-        pin_name6       : in     vl_logic;
-        pin_name8       : in     vl_logic;
-        pin_name9       : in     vl_logic;
-        pin_name10      : in     vl_logic;
-        pin_name11      : in     vl_logic;
-        pin_name12      : in     vl_logic;
-        pin_name13      : in     vl_logic;
-        pin_name14      : in     vl_logic;
-        pin_name15      : in     vl_logic;
-        pin_name16      : in     vl_logic;
-        pin_name17      : in     vl_logic;
-        pin_name18      : in     vl_logic;
-        pin_name19      : in     vl_logic;
-        pin_name20      : in     vl_logic;
+        P1_LEDS         : in     vl_logic_vector(0 to 8);
+        P2_LEDS         : in     vl_logic_vector(0 to 8);
         sampler_rx      : in     vl_logic
     );
 end Bloque_final_vlg_check_tst;
